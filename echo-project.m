ac660e9c7053f3da7614b95a46c909af1029f855
Gabriell Tavares <35067170+gtavaresl@users.noreply.github.com>
2020-07-14 20:47:42 -0300

    Add files via upload

--- echo-project.m
+++ echo-project.m
@@ -4,20 +4,20 @@ clear;
 
 
 %% Read audio from file
-[data, Fs] = audioread("audio.wav");
+[data, Fs] = audioread("audio_teste.ogg");
 
 % play the audio
 disp('playing audio');
 sound(data,Fs);
 
-%% Create audio recorder
+%% Record audio
 % Frame sample
 Fs = 44100;
 nBits = 16;
 % object
-recObj = audiorecorder(Fs, nBits);
+recObj = audiorecorder(Fs, nBits, 1);
 
-%% Record from microphone
+% Record from microphone
 t = 10; % audio time
 disp('Start recording');
 recordblocking(recObj, t);
@@ -33,14 +33,14 @@ data = getaudiodata(recObj);
 plot(data);
 
 %% Write audio
-audiowrite("audio_original.wav", data, Fs);
+audiowrite("audio_original.ogg", data, Fs);
 
 %% Add echo
 % add echo with amplitude a=0.4 after 0.2s
 a = 0.4;
 N = 0.2*Fs;
 
-data_echo(1:length(data)) = data(1:length(data));
+data_echo = data;
 
 for n = N+1:length(data)
     data_echo(n) = data(n) + a*data(n-N);
@@ -50,23 +50,34 @@ disp('Playing with echo');
 sound(data_echo, Fs);
 
 %% Remove echo
-% add echo with amplitude a=0.1 after 0.15s
-a = 0.3;
+% remove echo with amplitude a=0.4 after 0.2s
+a = 0.4;
 N = 0.2*Fs;
 
-data_without_echo(1:length(data)) = data_echo(1:length(data));
+data_without_echo = data_echo;
 
 for n = N+1:length(data_echo)
-    data_without_echo(n) = data_echo(n) - a*data_echo(n-N);
+    data_without_echo(n) = data_echo(n) - a*data_without_echo(n-N);
 end
 
 disp('Playing without echo');
 sound(data_without_echo, Fs);
 
-%% Correlation
+%% Plot data
 
-r = xcorr(data_without_echo, data);
-plot(r);
+t = [1:length(data)];
+
+plot(t,data_echo,'r',t,data,'b');
+title('Speech signals');
+xlabel('Samples');
+legend({'Echo','Original'},'Location','southwest')
+
+%% Diff
+
+diff = data_echo-data;
+plot(diff);
+title('Difference between audio and audio with echo');
+xlabel('Samples');
 
 %% Other way to add echo
 % add an echo after 0.15s (with amplitude=0.1, phase=pi/3.5) and echo after 0.3 seconds (with a=0.5 and phase=1.2*pi)
@@ -79,34 +90,29 @@ data_echo = real(conv(data,echo_vector));
 
 sound(data_echo, Fs);
 
-%% LMS filter
+%% NLMS filter
 
 h = zeros(1, length(data));
-step = 0.01;
-eps = 0.001;
+step = 0.02;
+eps = 0.002;
 mu = 1;
 
-not_echo(1:length(data)) = data_echo(1:length(data));
+data_without_echo = data_echo;
 
 for i=1:length(data)
     e(i) = data(i) - h(i)' * data_echo(i);
-    % NLMS
+    %NLMS
     mu = 1/(data_echo(i)'*data_echo(i) + eps);
     h(i+1) = h(i) + step * mu * e(i) * data_echo(i);
-    not_echo(i) = h(i+1)'*data_echo(i);
+    data_without_echo(i) = h(i+1)'*data_echo(i);
 end
 
-%not_echo = h(length(data))*data_echo;
 disp('Playing without echo');
-sound(not_echo, Fs);
+sound(data_without_echo, Fs);
 
 %% Plot error
 
-plot(sqrt(erro_LMS.*erro_LMS));
-title('Mean square error')
-
-hold on;
-
-plot(sqrt(erro_NLMS.*erro_NLMS));
-
-hold off;
+t = [1:length(data)];
+plot(t, sqrt(erro_LMS.*erro_LMS),t,sqrt(erro_NLMS.*erro_NLMS));
+title('Mean square error');
+xlabel('Samples');
